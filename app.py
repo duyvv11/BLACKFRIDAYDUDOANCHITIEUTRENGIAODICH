699ed0946bf55f9d4c0f28307283fb4caa13bd97
Le Quang Duy <quang125duy141@gmail.com>
2025-11-28 06:27:53 +0700

    ad

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load model đã train
-with open("rf_model.pkl", "rb") as f:
+with open("xgb_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route("/", methods=["GET", "POST"])
